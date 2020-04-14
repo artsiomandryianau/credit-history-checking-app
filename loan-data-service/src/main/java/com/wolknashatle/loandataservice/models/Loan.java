8f2a792181844133a2587f2b47a2dd2072b81190
artsiomandryianau <artsiom.andryianau@gmail.com>
2020-04-14 20:55:00 +0200

    loan-data-service modify properies

--- loan-data-service/src/main/java/com/wolknashatle/loandataservice/models/Loan.java
+++ loan-data-service/src/main/java/com/wolknashatle/loandataservice/models/Loan.java
@@ -2,6 +2,7 @@ package com.wolknashatle.loandataservice.models;
 
 
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
@@ -10,18 +11,18 @@ import javax.persistence.Id;
 public class Loan {
 
     @Id
-    private Integer loan_id;
-    private Integer client_id;
-    private boolean loan_is_payed;
+    private ObjectId loan_id;
+    private String client_id;
+    private String loan_is_payed;
     private String date_loan_take;
-    private Integer loan_number_month_back;
-    private Integer loan_amount;
+    private String loan_number_month_back;
+    private String loan_amount;
 
 
     public Loan() {
     }
 
-    public Loan(Integer loan_id, Integer client_id, boolean loan_is_payed, String date_loan_take, Integer loan_number_month_back, Integer loan_amount) {
+    public Loan(ObjectId loan_id, String client_id, String loan_is_payed, String date_loan_take, String loan_number_month_back, String loan_amount) {
         this.loan_id = loan_id;
         this.client_id = client_id;
         this.loan_is_payed = loan_is_payed;
@@ -30,27 +31,51 @@ public class Loan {
         this.loan_amount = loan_amount;
     }
 
-    public Integer getLoan_id() {
-        return loan_id;
+    public String getLoan_id() {
+        return loan_id.toHexString();
     }
 
-    public Integer getClient_id() {
+    public void setLoan_id(ObjectId loan_id) {
+        this.loan_id = loan_id;
+    }
+
+    public String getClient_id() {
         return client_id;
     }
 
-    public boolean isLoan_is_payed() {
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getLoan_is_payed() {
         return loan_is_payed;
+    }
+
+    public void setLoan_is_payed(String loan_is_payed) {
+        this.loan_is_payed = loan_is_payed;
     }
 
     public String getDate_loan_take() {
         return date_loan_take;
     }
 
-    public Integer getLoan_number_month_back() {
+    public void setDate_loan_take(String date_loan_take) {
+        this.date_loan_take = date_loan_take;
+    }
+
+    public String getLoan_number_month_back() {
         return loan_number_month_back;
     }
 
-    public Integer getLoan_amount() {
+    public void setLoan_number_month_back(String loan_number_month_back) {
+        this.loan_number_month_back = loan_number_month_back;
+    }
+
+    public String getLoan_amount() {
         return loan_amount;
+    }
+
+    public void setLoan_amount(String loan_amount) {
+        this.loan_amount = loan_amount;
     }
 }
--- loan-data-service/src/main/java/com/wolknashatle/loandataservice/repositories/config/MongoDbConfig.java
+++
@@ -1,8 +0,0 @@
-package com.wolknashatle.loandataservice.repositories.config;
-
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-@EnableMongoRepositories(basePackages = {"com.wolknashatle.loandataservice.repositories"})
-public class MongoDbConfig {
-
-}
