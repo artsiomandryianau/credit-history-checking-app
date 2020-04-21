35468be05fe1f6a9e5606ee4c8132be706fd0096
artsiomandryianau <artsiom.andryianau@gmail.com>
2020-04-21 17:50:04 +0200

    loger deleted noq is good version

--- API-gateway/src/main/java/com/wolknashatle/APIgateway/config/DataServiceConnectionConfig.java
+++ API-gateway/src/main/java/com/wolknashatle/APIgateway/config/DataServiceConnectionConfig.java
@@ -1,12 +1,8 @@
 package com.wolknashatle.APIgateway.config;
 
-import javafx.application.Application;
-import org.apache.logging.log4j.LogManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.logging.Logger;
 
 @Configuration
 public class DataServiceConnectionConfig {
--- API-gateway/src/main/java/com/wolknashatle/APIgateway/service/ConnectionService.java
+++ API-gateway/src/main/java/com/wolknashatle/APIgateway/service/ConnectionService.java
@@ -1,29 +1,24 @@
 package com.wolknashatle.APIgateway.service;
 
-import com.wolknashatle.APIgateway.config.DataServiceConnectionConfig;
 import com.wolknashatle.APIgateway.model.ClientInfo;
 import com.wolknashatle.APIgateway.model.LoanInfo;
 import com.wolknashatle.APIgateway.model.reports.OverdueLoansReport;
 import com.wolknashatle.APIgateway.model.reports.UnpaidLoansReport;
 import com.wolknashatle.APIgateway.properties.ApplicationProperties;
-import javafx.application.Application;
 import lombok.extern.log4j.Log4j2;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import com.wolknashatle.APIgateway.config.DataServiceConnectionConfig;
+
 import javax.validation.ConstraintViolationException;
 import java.util.Collections;
-import java.util.logging.Logger;
 
 
 @Service
 @Log4j2
 public class ConnectionService {
 
-    private static final Logger LOGGER = (Logger) LogManager.getLogger(Application.class);
+
 
     @Autowired
     private ApplicationProperties applicationProperties;
@@ -34,11 +29,9 @@ public class ConnectionService {
     public OverdueLoansReport getOverdueReport() {
         try {
             //Getting data from client-data-service!
-            LOGGER.info("try to connect to data collection service");
             String clientServiceUrl = "http://" + applicationProperties.getDataCollectionServiceHost() + ":" + applicationProperties.getDataCollectionServicePort();
             return restTemplate.getForObject(clientServiceUrl + "/getReport/overdueReport/", OverdueLoansReport.class);
         } catch (RuntimeException re) {
-            LOGGER.info("wrong id error");
             throw new ConstraintViolationException("error", Collections.emptySet());
 
         }
