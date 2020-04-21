73e8b433a18e74d3c025492e0b8914ba3f621e28
artsiomandryianau <artsiom.andryianau@gmail.com>
2020-04-21 17:45:25 +0200

    loger added

--- API-gateway/src/main/java/com/wolknashatle/APIgateway/config/DataServiceConnectionConfig.java
+++ API-gateway/src/main/java/com/wolknashatle/APIgateway/config/DataServiceConnectionConfig.java
@@ -1,13 +1,18 @@
 package com.wolknashatle.APIgateway.config;
 
+import javafx.application.Application;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Logger;
 
 @Configuration
 public class DataServiceConnectionConfig {
 
     @Bean
     public RestTemplate getRestTemplate() { return new RestTemplate();}
+
 
 }
