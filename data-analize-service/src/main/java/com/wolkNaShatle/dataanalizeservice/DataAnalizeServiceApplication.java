5a3ab8ab7bd8dcab3014a666cb0521fdfcb96bc0
artsiomandryianau <artsiom.andryianau@gmail.com>
2020-04-27 18:43:58 +0200

    data-analize-service added 2

---
+++ data-analize-service/src/main/java/com/wolkNaShatle/dataanalizeservice/DataAnalizeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wolkNaShatle.dataanalizeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAnalizeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAnalizeServiceApplication.class, args);
+	}
+
+}
---
+++ data-analize-service/src/test/java/com/wolkNaShatle/dataanalizeservice/DataAnalizeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wolkNaShatle.dataanalizeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAnalizeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
